5244fcef0c7c79c90df41189b70c4d184f55cd53
Dragos Tanta <dragos.tanta@gmail.com>
2022-04-19 12:36:06 +0300

    curs 1

--- src/curs1/FirstClass.java
+++ src/curs1/FirstClass.java
@@ -2,11 +2,20 @@ package curs1;
 
 public class FirstClass {
 
+	
 	public static void main(String[] args) {
 		
+		FirstClass obiect =  new FirstClass();
+		obiect.metoda();
 		
+		System.out.println(obiect.metoda());
 		System.out.println("Test");
 
+	}
+	
+	public String metoda() {
+		
+		return "Ceva";
 	}
 
 }
---
+++ src/curs1/Tester.java
@@ -0,0 +1,47 @@
+package curs1;
+
+public class Tester {
+	/**
+	 * Multiple line comment
+	 * awda
+	 * dawa
+	 */
+	//-> one line comment
+	//variabile
+	public String nume;
+	public int ratePerHour;
+	
+	//constructor
+	public Tester() {}
+	public Tester(String Prenume) {
+		nume = Prenume;
+	}
+	
+	
+	//metode
+	
+	public String getNume() {
+		return nume;
+	}
+	
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+	
+	
+	public int getRatePerHour() {
+		return ratePerHour;
+	}
+	
+	public void setRatePerHour(int ratePerHour) {
+		this.ratePerHour = ratePerHour;
+	}
+	
+
+	
+	
+	
+
+
+
+}
